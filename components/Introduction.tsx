ef6310d5ae5ee51430d7d02060f472322771e8a3
kewwie <iamkewwie@gmail.com>
2024-01-13 03:54:30 +0100

    added stuff

---
+++ components/Introduction.tsx
@@ -0,0 +1,8 @@
+export default function Introduction() {
+    return (
+        <div className="bg-white">
+           <h1 className="text-black">Kewwie</h1>
+           <h3 className="text-black">I manage and moderate different communities online.</h3>
+        </div>
+    )
+}
