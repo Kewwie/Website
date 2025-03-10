d8dcf4a1f2cd43ca7fd32db795c9a19d5679ceb5
Kewi <iamkewwie@gmail.com>
2025-03-10 18:32:07 +0100

    Update DISCORD_INVITE

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
 export const GITHUB_USERNAME = "kewwie";
 export const TWITTER_USERNAME = "iamkewi";
-export const DISCORD_INVITE = "https://discord.gg/kewi";
+export const DISCORD_INVITE = "https://discord.gg/JKbtSTCmbz";
 export const EMAIL_ADDRESS = "me@kewwie.com";
